fd5812d62186e9bb02e7999aeca37a9730d1ddac
Prasanth Reddy <prasanthreddy.maddala@gmail.com>
2021-04-18 23:12:49 +0530

    Blazor route parameters

--- LearnBlazor/EmployeeManagement.Web/Services/EmployeeService.cs
+++ LearnBlazor/EmployeeManagement.Web/Services/EmployeeService.cs
@@ -19,5 +19,10 @@ namespace EmployeeManagement.Web.Services
         {
             return await httpClient.GetJsonAsync<Employee[]>("api/employees");
         }
+
+        public async Task<Employee> GetEmployee(int id)
+        {
+            return await httpClient.GetJsonAsync<Employee>($"api/employees/{id}");
+        }
     }
 }
