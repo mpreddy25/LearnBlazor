4ee4c4a7bcc7818cddb537b059f5dc134cba38ff
Prasanth Reddy <prasanthreddy.maddala@gmail.com>
2021-04-18 16:49:05 +0530

    WebAPI Setup For Blazor Learning Sample Employee management API setup

---
+++ LearnBlazor/EmployeeManagement.Api/Models/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManagement.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Models
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetEmployees();
+        Task<IEnumerable<Employee>> Search(string name, Gender? gender);
+        Task<Employee> GetEmployee(int employeeId);
+        Task<Employee> GetEmployeeByEmail(string email);
+        Task<Employee> AddEmployee(Employee employee);
+        Task<Employee> UpdateEmployee(Employee employee);
+        Task<Employee> DeleteEmployee(int employeeId);
+    }
+}
--- LearnBlazor/EmployeeManagement.Api/Startup.cs
+++ LearnBlazor/EmployeeManagement.Api/Startup.cs
@@ -1,7 +1,9 @@
+using EmployeeManagement.Api.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,10 @@ namespace EmployeeManagement.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
